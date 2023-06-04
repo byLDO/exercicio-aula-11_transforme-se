cd936361d8753db3b14d4d93424ccd96dca2cc39
Helido <byhelido4@gmail.com>
2023-06-04 20:52:07 -0300

    exercicio 11
    Criação de enum

---
+++ MeuArquivo.java
@@ -0,0 +1,14 @@
+
+public class MeuArquivo {
+    public static void main(String[] args) {
+
+    }
+}
+
+public enum EstadoCivil {
+    SOLTEIRO,
+    CASADO,
+    SEPARADO,
+    DIVORCIADO,
+    VIUVO,
+}
